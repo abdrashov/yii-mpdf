47de2ea72384465855de8a5e03d329cbfdb05dd8
abdrashov <abdrashovv@gmail>
2024-04-19 20:17:24 +0500

    first commit

---
+++ src/YiiMpdfWrapper.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Abdrashov\YiiMpdf;
+
+//use Illuminate\Support\Facades\View;
+use Abdrashov\YiiMpdf\YiiMpdf as Pdf;
+use Yii;
+
+class YiiMpdfWrapper
+{
+    /**
+     * @param array $config optional, default []
+     * @return YiiMpdf
+     */
+    public function getPdf($config = [])
+    {
+        return new YiiMpdf($config);
+    }
+
+    /**
+     * Load a HTML string
+     *
+     * @param string $html
+     * @param array $config optional, default []
+     * @return Pdf
+     * @throws \Mpdf\MpdfException
+     */
+    public function loadHTML($html, $config = [])
+    {
+        $pdf = $this->getPdf($config);
+        $pdf->getMpdf()->WriteHTML($html);
+
+        return $pdf;
+    }
+
+    /**
+     * Chunk a HTML with given word and load string
+     *
+     * @param string $separator
+     * @param string $html
+     * @param array $config optional, default []
+     * @return Pdf
+     * @throws \Mpdf\MpdfException
+     */
+    public function chunkLoadHTML($separator, $html, $config = [])
+    {
+        $pdf = $this->getPdf($config);
+
+        $chunks = explode($separator, $html);
+        foreach ($chunks as $chunk) {
+            $pdf->getMpdf()->WriteHTML($chunk);
+        }
+
+        return $pdf;
+    }
+
+    /**
+     * Load a HTML file
+     *
+     * @param string $file
+     * @param array $config optional, default []
+     * @return Pdf
+     * @throws \Mpdf\MpdfException
+     */
+    public function loadFile($file, $config = [])
+    {
+        return $this->loadHTML(file_get_contents($file), $config);
+    }
+
+    /**
+     * Chunk a HTML file with given word and load HTML
+     *
+     * @param string $separator
+     * @param string $file
+     * @param array $config optional, default []
+     * @return Pdf
+     */
+    public function chunkLoadFile($separator, $file, $config = [])
+    {
+        return $this->chunkLoadHTML($separator, file_get_contents($file), $config);
+    }
+
+    /**
+     * Load a View and convert to HTML
+     *
+     * @param string $view
+     * @param array $data
+     * @param array $config optional, default []
+     * @return Pdf
+     * @throws \Mpdf\MpdfException
+     */
+    public function loadView($view, $data = [], $config = [])
+    {
+        return $this->loadHTML(Yii::$app->view->renderFile($view, $data), $config);
+    }
+
+    /**
+     * Chunk a View with given word and load HTML
+     *
+     * @param string $separator
+     * @param string $view
+     * @param array $data
+     * @param array $config optional, default []
+     * @return Pdf
+     * @throws \Mpdf\MpdfException
+     */
+    public function chunkLoadView($separator, $view, $data = [], $config = [])
+    {
+        return $this->chunkLoadHTML($separator, Yii::$app->view->renderFile($view, $data), $config);
+    }
+}
